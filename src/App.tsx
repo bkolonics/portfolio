5909bfc6256404251ad33734e6d50f6fab07d965
Bence KOLONICS <placeholder@minet.net>
2023-05-06 22:35:42 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-violet-color">App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "background-color": "#f0f7ee",
+        "violet-color": "#8390fa",
+      },
+    },
+  },
+  plugins: [],
+};
