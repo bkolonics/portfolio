8532f2eb61ce3fbdb4c743316a4f6f6298dc3831
Bence KOLONICS <placeholder@minet.net>
2023-05-08 23:04:08 +0200

    Adding responsive design to widget component

--- src/Components/Widget.tsx
+++ src/Components/Widget.tsx
@@ -1,26 +1,26 @@
 export function Widget() {
     return (
       <div className="flex flex-col items-center animate-fade-in">
-        <p className="text-vista-blue text-9xl">
-          BK
+        <p className="text-vista-blue text-4xl md:text-7xl lg:text-9xl">Bence Kolonics</p>
+        <p className="text-vista-blue text-3xl md:text-4xl lg:text-5xl animate-wiggle">
+          Work in progress
         </p>
-        <p className="text-vista-blue text-5xl animate-wiggle">Work in progress</p>
-        <div className="flex gap-8 mt-5">
+        <div className="flex gap-5 lg:gap-8 mt-3 lg:mt-5">
           <svg
             viewBox="0 0 100 100"
-            className="fill-vista-blue h-5 animate-[pulse_2s_infinite_1s]"
+            className="fill-vista-blue h-4 lg:h-5 animate-[pulse_2s_infinite_1s]"
           >
             <circle cx="50" cy="50" r="50" />
           </svg>
           <svg
             viewBox="0 0 100 100"
-            className="fill-vista-blue h-5 animate-[pulse_2s_infinite_1500ms]"
+            className="fill-vista-blue h-4 lg:h-5 animate-[pulse_2s_infinite_1500ms]"
           >
             <circle cx="50" cy="50" r="50" />
           </svg>
           <svg
             viewBox="0 0 100 100"
-            className="fill-vista-blue h-5 animate-[pulse_2s_infinite_2s]"
+            className="fill-vista-blue h-4 lg:h-5 animate-[pulse_2s_infinite_2s]"
           >
             <circle cx="50" cy="50" r="50" />
           </svg>
