97895505dbbdfba11757ad87c8b6ab0177c7c4c7
Bence KOLONICS <placeholder@minet.net>
2023-05-08 13:42:41 +0200

    Finished widget v1 whith animations...

--- src/Components/Widget.tsx
+++ src/Components/Widget.tsx
@@ -1,18 +1,27 @@
 export function Widget() {
     return (
       <div className="flex flex-col items-center animate-fade-in">
-        <p className="text-vista-blue text-9xl animate-[spin_10s_ease-in-out_infinite]">
+        <p className="text-vista-blue text-9xl">
           BK
         </p>
-        <p className="text-vista-blue text-5xl">Work in progress</p>
-        <div className="flex gap-8 mt-5 animate-bounce">
-          <svg viewBox="0 0 100 100" className="fill-vista-blue h-5">
+        <p className="text-vista-blue text-5xl animate-wiggle">Work in progress</p>
+        <div className="flex gap-8 mt-5">
+          <svg
+            viewBox="0 0 100 100"
+            className="fill-vista-blue h-5 animate-[pulse_2s_infinite_1s]"
+          >
             <circle cx="50" cy="50" r="50" />
           </svg>
-          <svg viewBox="0 0 100 100" className="fill-vista-blue h-5">
+          <svg
+            viewBox="0 0 100 100"
+            className="fill-vista-blue h-5 animate-[pulse_2s_infinite_1500ms]"
+          >
             <circle cx="50" cy="50" r="50" />
           </svg>
-          <svg viewBox="0 0 100 100" className="fill-vista-blue h-5">
+          <svg
+            viewBox="0 0 100 100"
+            className="fill-vista-blue h-5 animate-[pulse_2s_infinite_2s]"
+          >
             <circle cx="50" cy="50" r="50" />
           </svg>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,15 @@ export default {
         "fade-in": {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
-        }
+        },
+        "wiggle": {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
       },
       animation: {
         "fade-in": "fade-in 0.5s ease-out",
+        "wiggle": "wiggle 1s ease-in-out infinite",
       },
     },
   },
