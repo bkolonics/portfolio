da08ffcb4f91522de7ac3a948d9499fabafc3831
Bence KOLONICS <placeholder@minet.net>
2023-05-10 15:02:33 +0200

    Added function to calculate age

--- src/Components/Description.tsx
+++ src/Components/Description.tsx
@@ -1,7 +1,21 @@
 import photo from '../assets/photo.jpg';
 
+interface DescriptionProps {
+    age: number;
+}
 
-export function Description() {
+function calculateMyAge() {
+    const today = new Date();
+    const birthDate = new Date(2001, 3, 11);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) age--;
+    return age;
+}
+
+export function Description({age}: DescriptionProps) {
+
+    age = calculateMyAge();
     return (
       <>
         <div className="flex">
@@ -16,7 +30,8 @@ export function Description() {
             <h1 className="text-4xl font-bold text-center">
               Hi, I'm <span className="text-Glaucous">Bence</span> <br />
               <span className="text-3xl font-semibold text-center">
-                I'm a <span className="text-Glaucous"> age </span> Developer
+                I'm a <span className="text-Glaucous"> {age}</span> years
+                old Developer
               </span>
             </h1>
           </div>
