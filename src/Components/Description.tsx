cb3e33ab48dee96f1651cf82abec35d51984e8f6
Bence KOLONICS <placeholder@minet.net>
2023-05-10 19:05:36 +0200

    Landing page nearly done TODO: responsive

--- src/Components/Description.tsx
+++ src/Components/Description.tsx
@@ -18,20 +18,26 @@ export function Description({age}: DescriptionProps) {
     age = calculateMyAge();
     return (
       <>
-        <div className="flex">
-          <div className="w-1/2">
+        <div className="flex flex-col lg:flex-row">
+          <div className="lg:w-1/2">
             <img
               src={photo}
               alt="photo"
-              className="rounded-full w-6/12 justify-center items-center block mx-auto"
+              className="rounded-full w-6/12 justify-center items-center block mx-auto border-8 border-Glaucous shadow-2xl"
             />
           </div>
-          <div className="w-1/2">
-            <h1 className="text-4xl font-bold text-center">
-              Hi, I'm <span className="text-Glaucous">Bence</span> <br />
-              <span className="text-3xl font-semibold text-center">
-                I'm a <span className="text-Glaucous"> {age}</span> years
-                old Developer
+          <div className="lg:w-1/2">
+            <h1 className="text-3xl lg:text-4xl font-bold text-center">
+              Hi, I'm <span className="text-Glaucous">Bence</span>
+            </h1>
+            <h1 className="text-justify md:m-20 m-5">
+              <span className="text-2xl lg:text-3xl font-semibold text-center">
+                I'm a <span className="text-Glaucous"> {age}</span> years old
+                Developer. Molestiae tenetur ipsum voluptas qui rerum
+                praesentium dolores. Omnis occaecati quod ea a. Veritatis animi
+                laudantium delectus autem quia. Qui totam aut itaque et
+                voluptatem eum illo odit. Voluptatem nostrum impedit
+                reprehenderit. Nesciunt distinctio enim illo et.
               </span>
             </h1>
           </div>
