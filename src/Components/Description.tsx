7f5b60b09537833b01d45df366a3d93481edeb53
Bence KOLONICS <placeholder@minet.net>
2023-05-21 22:50:42 +0200

    Serving image in different sizes

--- src/Components/Description.tsx
+++ src/Components/Description.tsx
@@ -1,4 +1,9 @@
 import photo from '../assets/photo.webp';
+import photoS from '../assets/photos/photoS.webp';
+import photoM from '../assets/photos/photoM.webp';
+import photoL from '../assets/photos/photoL.webp';
+import photoXL from '../assets/photos/photoXL.webp';
+import photoXXL from '../assets/photos/photoXXL.webp';
 import { FC, PropsWithChildren, useEffect, useRef, useState } from "react";
 import { RefObject } from "react";
 import { useTranslation } from "react-i18next";
@@ -80,6 +85,16 @@ export function Description() {
                 src={photo}
                 alt="photo"
                 className="rounded-image w-6/12 justify-center items-center block mx-auto xl:border-image border-mobile border-Glaucous shadow-2xl"
+                srcSet={`${photoS} 256w,
+                        ${photoM} 690w,
+                        ${photoL} 980w,
+                        ${photoXL} 1200w,
+                        ${photoXXL} 1380w`}
+                sizes="(max-width: 256px) 256px,
+                        (max-width: 690px) 690px,
+                        (max-width: 980px) 980px,
+                        (max-width: 1200px) 1200px,
+                        1380px"
               />
             </AnimateLeft>
           </div>
